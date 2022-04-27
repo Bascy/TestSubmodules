9e6337a7152c7c23640cea1aa82a00bbd41b7c14
Bascy <basschouten65@gmail.com>
2022-04-27 12:15:25 +0200

    Added lib1 submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
+#include "SPIFFS.h"
+#include "Lib1.h"
+#include "Lib2.h"
 
+Lib1 l1;
+Lib2 l2;
 
 void setup() {
   // put your setup code here, to run once:
+  SPIFFS.begin();
 }
 
 void loop() {
