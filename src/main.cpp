5ac8d50d006ddb83652fd9f669fbeb82de07e0de
Bascy <basschouten65@gmail.com>
2022-04-27 12:21:08 +0200

    Added lib2 as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include "SPIFFS.h"
 #include "Lib1.h"
 #include "Lib2.h"
+#include "Lib3.h"
 
 Lib1 l1;
-Lib2 l2;
+Lib2 l2a;
+Lib3 l3;
 
 void setup() {
   // put your setup code here, to run once:
